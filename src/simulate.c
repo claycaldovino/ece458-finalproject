a4b704a606a42827e69efd436018d2a7bc975c6e
Clay Caldovino <cc5@pdx.edu>
2016-02-06 23:23:56 -0800

    Added error checking for input file.

--- src/simulate.c
+++ src/simulate.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// DRAM timing constraints
+// DRAM timing constraints (Need later)
 
 #define tRC 50
 #define tRAS 36
@@ -16,55 +16,61 @@
 #define tBURST 4
 #define tWTR 8
 
+struct instruction 
+{
+char name[32];
+char address[32];
+int age;
+};
+
+
 int main(int argc, char **argv)
 {
- 
-        typedef struct
-        {
-        char name[32];
-        char address[32];
-        int age;
-        } instruction;
-
-        instruction commandQueue[15];
+        struct instruction commandQueue[16];  
         FILE * fp;
-
         char buf[128];
         char commandFile[64];
-        char *item;
+        char *token;
         int openSlot = 0;
         int i;
 
         if (argc != 2)
         {
-                printf("You must enter a testfile to use, 'simulate testfile.txt'\n");
+                printf("Proper usage: './a.out testfile.txt'\n");
                 return 1;
         }
 
-        strcpy(commandFile, argv[1]);
-        fp = fopen(commandFile,"r");
-
-        while (fgets(buf,128,fp))
+        if ((fp = fopen(argv[1],"r")) == NULL)
         {
+                printf("Could not open %s\n", argv[1]);
+                return 1;
+        }
 
-                item = strtok(buf,"\t");
-                strcpy(commandQueue[openSlot].address,item);
+        while (fgets(buf, sizeof(buf), fp))
+        {
+                printf("%s", buf);
+                token = strtok(buf," \t");
+                strcpy(commandQueue[openSlot].address, token);
 
-                item = strtok(NULL,"\t");
-                strcpy(commandQueue[openSlot].name,item);
-
-                item = strtok(NULL,"\n");
-                commandQueue[openSlot].age = atoi(item);
-
-                printf("%s\n",commandQueue[openSlot].address);
+                token = strtok(NULL," \t");
+                strcpy(commandQueue[openSlot].name, token);
+                
+                token = strtok(NULL," \t\n");
+                commandQueue[openSlot].age = atoi(token);
+                
                 openSlot++;
         }
 
         fclose(fp);
 
-        for (i=0; i<openSlot; i++) 
+//
+// for testing
+//
+        
+
+        for (i = 0; i < openSlot; i++) 
         {
-                printf("slot #%d\n",i);
+                printf("\nslot #%d\n",i);
                 printf("name: %s\n",commandQueue[i].name);
                 printf("address: %s\n",commandQueue[i].address);
                 printf("age: %d\n",commandQueue[i].age);
