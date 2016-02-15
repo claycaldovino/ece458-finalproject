e933917203f01903329d6de1bfc646985e6382c7
Taylor <griffin.taylorb@gmail.com>
2016-02-15 11:57:48 -0800

    Use these structs!

---
+++ src/Struct_Info.c
@@ -0,0 +1,32 @@
+// Team TTSC
+// ECE 485
+
+// Structs!
+
+// Command Struct
+
+int CPU_Time;
+
+struct Command {
+
+	// Address Info
+	int Full_Adress;
+	int Row;
+	int Bank;
+	int Column;
+	
+	char Name[32];
+	int Time_Issued;
+	
+	int Full;
+}
+
+struct DIMM_Status{
+	
+	int Last_Row;
+	int Last_Bank;
+	int Last_Column;
+	
+	char Last_Command[32];	
+}
+
