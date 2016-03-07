7a50ebafa55e6b142e722c5305d3c50f7d77f5ab
Todd Harlow <tharlow@pdx.edu>
2016-03-06 20:13:03 -0800

    updateTimers.h addition

--- src/updateTimers.h
+++ src/updateTimers.h
@@ -1,3 +1,13 @@
+void initializeTimers()
+{
+	int i,j;
+	
+	for (i = 0; i < TOTAL_BANKS; ++i)
+		for (j = 0; j < 4; ++j)
+			commandTimers[i][j] = 200;	
+	
+}
+
 void updateTimers(command cmd, unsigned bank)
 {
 	int i, j;
